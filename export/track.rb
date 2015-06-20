e9ea1dbfac2d25976e722d9204676daff1c2933e
Brendan Caffrey <brendan@jcaffrey.com>
2015-06-20 00:10:47 -0500

    use the config value

--- export/track.rb
+++ export/track.rb
@@ -2,8 +2,9 @@ module Export
   class Track < Struct.new(:id, :name, :sort_name, :artist, :sort_artist, :album, :sort_album,
     :genre, :duration, :start, :finish, :track, :track_count, :disc, :disc_count, :play_count, :location)
     def file
-      # TODO use config value here
-      location.split(':', 2)[1].gsub(/^Users:.*?:Music:/, '').gsub(':', '/')
+      # location is like "Macintosh HD:Users:Brendan:Music:iTunes:iTunes Music:artist:album:song.mp3"
+      # so we turn the : into /, remove the drive name and lop off what the config provides
+      location.gsub(':', '/').gsub(/^.+?\//, '/').gsub(Config['music_path'], '')
     end
   end
 end
