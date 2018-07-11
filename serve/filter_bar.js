e8ad4e82ffff40dcb451a6627c265b45e3a21d25
Brendan Caffrey <brendan@jcaffrey.com>
2018-07-10 19:42:38 -0500

    bootstrap styling and an icon for the filter bar

--- serve/filter_bar.js
+++ serve/filter_bar.js
@@ -1,8 +1,9 @@
 var FilterBar = function() {
-  var id = "#filter";
-  $(id).append("<label>Search: <input type=\"search\" /></label>");
-  this.input = $(id).find("input");
-  // the "input" event fires every time the value changes, but the "change" event only fires when "committed"?
+  var formGroup = $("<div class=\"form-group has-feedback has-feedback-left\"></div>").appendTo("#filter")
+  this.input = $("<input type=\"search\" id=\"filter-bar\" class=\"form-control input-sm\" placeholder=\"Search\" />").appendTo(formGroup);
+  $("<i class=\"form-control-feedback icon ion-ios-search\">").appendTo(formGroup);
+
+  // the "input" event fires every time the value changes, but the "change" event only fires when "committed"
   this.input.on('input', this.textChanged.bind(this));
 }
 
